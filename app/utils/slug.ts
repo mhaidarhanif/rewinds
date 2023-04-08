a151ffc9f862c9473d2ebdf4b614912340993ec7
M Haidar Hanif <git@mhaidarhanif.com>
2023-04-08 21:59:32 +0700

    docs: slug and vscode settings

--- app/utils/slug.ts
+++ app/utils/slug.ts
@@ -1,5 +1,10 @@
 import voca from "voca";
 
+/**
+ * Voca is a JavaScript library for manipulating strings
+ * So it's not just to slugify text
+ */
+
 export function createSlug(text: string) {
   return voca.slugify(text);
 }
