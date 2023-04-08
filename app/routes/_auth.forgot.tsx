11145e1463851d33230131fac69216e2cff9f37c
M Haidar Hanif <git@mhaidarhanif.com>
2023-04-08 22:13:30 +0700

    feat: auth forgot and reset route

---
+++ app/routes/_auth.forgot.tsx
@@ -0,0 +1,5 @@
+// Example: https://substack.com/account/forgot?email=name@example.com
+
+export default function Route() {
+  return null;
+}
---
+++ app/routes/_auth.reset.tsx
@@ -0,0 +1,5 @@
+// Example: https://substack.com/account/reset
+
+export default function Route() {
+  return null;
+}
